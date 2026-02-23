269c10f95453744e94c51827ffa748d76eea0ebd
bmelene <bmelene@gmail.com>
2026-02-23 16:02:50 +0000

    Changed it into a function return a list of n primes.

--- enieme_premier.py
+++ enieme_premier.py
@@ -1,30 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-#################################################################
-#                                                               #
-# Auteur :  Boris Mélène                                        #
-# Date :    27/06/2020                                          #
-# Version : 1.6.1 Linux                                         #
-# But : Calcule le n-ème nombre premier et retourne le nombre   #
-#       de tests nécessaires pour arriver au résultat.          #
-#                                                               #
-#                                                               #
-#                                                               #
-#                                                               #
-# Mises-à-jour :                                                #
-#  1.3.5 Version initiale. Copie et mise-à-jour pour Linux de   #
-#  la version pour Windows.                                     #
-#  1.4.0 Réduit le nombre de valeurs à tester.                  #
-#  1.4.1 Réarrange les declarations de variables.               #
-#  1.4.2 Conversion en Python                                   #
-#  1.5.0 Mise-à-jour pour Python 3                              #
-#  1.6.0 Utilisation de générateurs                             #
-#  1.6.1 Added utf-8 encoding                                   #
-#                                                               #
-#                                                               #
-#################################################################
-#
 
 from time import localtime 
 from math import sqrt
@@ -43,58 +19,64 @@ def gen_candidat():
         j += 1
         yield p
 
+def n_prems(num:int):
+    compteur = 1
+    prem_test = []
+    premiers = [2,3,5,7]
+    i = 3
+    c = gen_candidat()
+    nb_test = 0
+
+    while compteur < num-3:
+        # Sélection du nombre à tester que l'on appelle : le "candidat".
+        # On suppose arbitrairement qu'il est premier : check = True.
+        candidat = next(c)
+        check = True    
+
+        # Tests pour savoir si le candidat est premier.
+        # On fait le teste racine, qui s'il réussit, nous indique que le candidat n'est pas premier.
+        # Il permet également d'augmenter la liste de nombres premier test.
+        if premiers[i] == sqrt(candidat):
+            nb_test += 1 
+            prem_test.append(premiers[i])
+            check = False
+            i += 1 
+        else: 
+            for r in prem_test: 
+                nb_test += 1  
+                if candidat % r == 0:  
+                    check = False
+                    break
+        if check:
+            premiers.append(candidat) 
+            compteur += 1     
+
+    return premiers,nb_test
 
 ### Main
 
-compteur = 1
-nb_test = 0
-prem_test = []
-premiers = [2,3,5,7]
-i = 3
-c = gen_candidat()
+if __name__ == "__main__":
 
-if len(argv)>1:
-    numero = int(argv[1])
-else:
-    numero = int(input("Quel nombre premier voulez-vous ? Le numéro : "))
+    if len(argv)>1:
+        numero = int(argv[1])
+    else:
+        numero = int(input("Quel nombre premier voulez-vous ? Le numéro : "))
 
-time = datetime.now()
-time1 = 3600*time.hour + 60*time.minute + time.second
+    time = datetime.now()
+    time1 = 3600*time.hour + 60*time.minute + time.second
 
-while compteur < numero-3:
-    # Sélection du nombre à tester que l'on appelle : le "candidat".
-    # On suppose arbitrairement qu'il est premier : check = True.
-    candidat = next(c)
-    check = True    
+    premiers,nb_test = n_prems(numero)
 
-    # Tests pour savoir si le candidat est premier.
-    # On fait le teste racine, qui s'il réussit, nous indique que le candidat n'est pas premier.
-    # Il permet également d'augmenter la liste de nombres premier test.
-    if premiers[i] == sqrt(candidat):
-        nb_test += 1 
-        prem_test.append(premiers[i])
-        check = False
-        i += 1 
-    else: 
-        for r in prem_test: 
-            nb_test += 1  
-            if candidat % r == 0:  
-                check = False
-                break
-    if check:
-        premiers.append(candidat) 
-        compteur += 1 
+    time = datetime.now()
+    time2 = 3600*time.hour + 60*time.minute + time.second
+    secondes = time2 - time1
 
-time = datetime.now()
-time2 = 3600*time.hour + 60*time.minute + time.second
-secondes = time2 - time1
+    print(f"\nLe {numero}ème nombre premier est {premiers[numero-1]}")
+    print(f"Nombre de tests : {nb_test}")
+    print(f"Temps (s) = {secondes}")
 
-print(f"\nLe {numero}ème nombre premier est {premiers[numero-1]}")
-print(f"Nombre de tests : {nb_test}")
-print(f"Temps (s) = {secondes}")
-
-if (secondes):
-    testsec = round(nb_test / secondes)
-    print(f"Tests/s : {testsec}")
+    if (secondes):
+        testsec = round(nb_test / secondes)
+        print(f"Tests/s : {testsec}")
 
 
