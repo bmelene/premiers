467cf97ea8015e8cf1a025474c181ae0d3658569
bmelene <bmelene@gmail.com>
2026-02-24 16:41:53 +0000

    Added decorator.

--- enieme_premier.py
+++ enieme_premier.py
@@ -6,8 +6,20 @@ from time import localtime
 from math import sqrt
 from datetime import datetime
 from sys import argv
+from functools import wraps
 
 ### Functions
+
+def time_fct(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        global seconds
+        time_start = datetime.now()
+        result = func(*args, **kwargs)
+        time_end = datetime.now()
+        seconds = (time_end - time_start).total_seconds()
+        return result
+    return wrapper
 
 def gen_candidat():
     increment = (4,2,4,2,4,6,2,6)
@@ -19,6 +31,7 @@ def gen_candidat():
         j += 1
         yield p
 
+@time_fct
 def n_prems(num:int):
     compteur = 1
     prem_test = []
@@ -62,21 +75,14 @@ if __name__ == "__main__":
     else:
         numero = int(input("Quel nombre premier voulez-vous ? Le numéro : "))
 
-    time = datetime.now()
-    time1 = 3600*time.hour + 60*time.minute + time.second
-
     premiers,nb_test = n_prems(numero)
-
-    time = datetime.now()
-    time2 = 3600*time.hour + 60*time.minute + time.second
-    secondes = time2 - time1
 
     print(f"\nLe {numero}ème nombre premier est {premiers[numero-1]}")
     print(f"Nombre de tests : {nb_test}")
-    print(f"Temps (s) = {secondes}")
+    print(f"Temps (s) = {seconds}")
 
-    if (secondes):
-        testsec = round(nb_test / secondes)
+    if (seconds):
+        testsec = round(nb_test / seconds)
         print(f"Tests/s : {testsec}")
 
 
